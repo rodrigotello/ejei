a9c778181636dd4d309b22e485f154d0ffb010e0
rodrigotello <rodrigotello@outlook.com>
2013-11-27 17:34:50 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ejei::Application.config.secret_token = 'b11b2fc6b8f32b17eeb8b6b16e76f2104c83150d313d2ea2cb701756e4beb29a002aa099d0eaaf61ec5fcd6e82616398b66865e18d92e21bcee32910117e7a1e'
