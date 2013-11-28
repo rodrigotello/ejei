8c4f7bf95d56203927806c8511ebc24983b2bcf0
rodrigotello <rodrigotello@outlook.com>
2013-11-27 18:39:59 -0600

    Landing page running

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Ejei::Application.routes.draw do
+  root :to => 'static_pages#home'
+
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
